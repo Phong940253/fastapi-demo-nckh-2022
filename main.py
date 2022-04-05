cf6c2c4500dc1e21c21580effae7a2028a78faaf
Phong <phong940253@gmail.com>
2022-04-05 11:38:58 +0700

    add api get page, group, post of page, post of group, content post

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 BASEURL = "https://graph.facebook.com"
 VERSION = "v13.0"
-data_request = {
+payload = {
     'access_token': TOKEN,
     'debug': 'all',
     'format': 'json',
@@ -25,8 +25,36 @@ data_request = {
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/get-post-group/{page_id}")
-async def get_post_group(page_id):
-    r = requests.get(BASEURL + "/" + VERSION + "/" + page_id, params=data_request)
-    print(r.url)
-    return r.json()
+@app.get("/pages")
+async def get_list_pages():
+    url = BASEURL + "/" + VERSION + "/me/feed"
+    response = requests.get(url, params=payload)
+    return response.json()
+
+@app.get("/groups")
+async def get_list_groups():
+    url = BASEURL + "/" + VERSION + "/me"
+    payload['fields'] = "groups"
+    response = requests.get(url, params=payload).json()
+    res = response['groups']['data']
+    response = response['groups']
+    while 'paging' in response and 'next' in response['paging']:
+        response = requests.get(response['paging']['next']).json()
+        res = res + response['data']
+    return res
+
+@app.get("/page/{page_id}")
+async def get_list_post_page(page_id: str):
+    response = requests.get(BASEURL + '/' + VERSION + '/' + page_id, params=payload)
+    return response.json()
+
+@app.get("/group/{group_id}")
+async def get_list_post_group(group_id: str):
+    response = requests.get(BASEURL + '/' + VERSION + '/' + group_id, params=payload)
+    return response.json()
+
+@app.get("/post/{post}")
+async def get_post_group(post: str):
+    payload['fields'] = 'feed{comments{comments,message},message}'
+    response = requests.get(BASEURL + "/" + VERSION + "/" + post, params=payload)
+    return response.json()
